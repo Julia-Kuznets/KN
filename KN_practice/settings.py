abb14bd1825d8288206dcdd373568dc3084538d8
Name <2238812@mail.ru>
2025-04-27 04:50:06 +0300

    Configure Django allowed_hosts

--- KN_practice/settings.py
+++ KN_practice/settings.py
@@ -33,7 +33,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0') == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
